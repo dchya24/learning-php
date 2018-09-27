6c78a122537df1bcfd78b1c48f05f302f658e51a
dchya24 <cahyadinar241@gmail.com>
2018-09-27 12:42:24 +0700

    make model class

--- core/Model.php
+++ core/Model.php
@@ -17,6 +17,11 @@ class Model {
 
     public $_mysqli;
 
+    /**
+     * define a primary key
+     */
+    protected $primaryKey = 'id';
+
     protected $_column = '*';
     protected $_order = 'ORDER BY';
 
@@ -24,23 +29,15 @@ class Model {
 
     private  $_connection;
     private  $_instance; // the single instance
+    private $_query = "SELECT *";
 
-    // private $env;
+    private $_operator = ['.', "=", "=>", "=<", "<", ">", "LIKE", "not like"];
 
 
     /**
-     * Get an Instance of the database
-     * @return Instance
+     * Construct
+     * define a table
      */
-    public function getInstance(){
-        if(!$this->_instance){
-            $this->_instance = new self();
-        }
-
-        return $this->_instance;
-    }
-
-    // construct
     public function __construct(){
         if($this->table == null){
             $className = substr(get_called_class(), strrpos(get_called_class(), '\\') + 1);
@@ -59,14 +56,24 @@ class Model {
 
     }
 
+    /**
+     * get all data form table
+     * return array
+     */
     public function all(){
-        return $this->_connection->query('SELECT * FROM users')->fetch_all();
+        return $this->_connection->query("SELECT * FROM {$this->table}")->fetch_all();
+        // return "SELECT * FROM {$this->table}";
     }
 
     /**
      * func for select column
+     * need param
+     * @param array 
+     * except (['id', 'name'])
+     * or
+     * @param string except ('name)
      */
-    public function select($arr){
+    public function select($arr = '*'){
         $this->_column = "";
 
         if(is_array($arr)){
@@ -74,32 +81,72 @@ class Model {
             foreach($arr as $key){
                 if($key != end($arr)){
                     $this->_column .= $key . ', ';
+                    $this->_query .= $key . ', ';
                 }
                 else if($key == end($arr)){
                     $this->_column .= $key;
+                    $this->_query .= $key;
                 }    
             }
 
         }else{
             $this->_column .= $arr;
+            $this->_query .= $arr;
+
         }
 
 
+
         return $this;
     }
 
-    public function where($arg){
+    public function where(array $arg){
+        if($this->_whereClause == null){
+            $this->makeWhere($arg, "WHERE");
+        }else{
+            $this->makeWhere($arg, " AND");
+        }
+
+        // return (array_search($arg[1], $this->_operator));
         return $this;
     }
 
+    private function  makeWhere(array $arg, string $string) {
+        if(count($arg)){
+            if(!empty(array_search($arg[1], $this->_operator)) ){
+                if($arg[1] == "LIKE"){
+                    $this->_whereClause .= "$string {$arg[0]} {$arg[1]} '%{$arg[2]}%'";                    
+                }else{
+                    $this->_whereClause .= "$string {$arg[0]} {$arg[1]} {$arg[2]}";
+                }
+            }   
+        }else{
+            $this->_whereClause .= "WHERE {$arg[0]} = {$arg[1]}";
+        }
+    }
+
+    /**
+     * get data
+     */
     public function get(){
 
-        $query = "SELECT {$this->_column} FROM {$this->table}";
+        $this->_query = "$this->_query FROM $this->table $this->_whereClause";
 
-        $data = $this->_connection->query($query)->fetch_all();
+        $data = $this->_connection->query($this->_query)->fetch_all();
 
         return $data;
     }
 
+    public function find($param){
+        $query = "SELECT * FROM {$this->table} WHERE {$this->primaryKey} ".'='. " $param LIMIT 1";
+
+        $data = $this->_connection->query($query)->fetch_all();
+
+        return $data[0];
+    }
+
+    public function insert(array $array){
+        
+    }
 
 }
