2d65acf739c25a4eab8741c79f760116fe03bd98
dchya24 <cahyadinar241@gmail.com>
2018-10-09 20:09:34 +0700

    Fixing up

--- core/Router.php
+++ core/Router.php
@@ -1,78 +1,71 @@
-<?php
+<?php 
 
 namespace Core;
 
-/**
- * Router
- *
- * PHP version 7.0
- */
-class Router
+use Core\Helper;
+
+Class Router 
 {
+ /**
+  * Array of Routes
+  * @var array
+  */
 
-    /**
-     * Associative array of routes (the routing table)
-     * @var array
-     */
-    protected $routes = [];
+  protected $routes  = [];
 
-    /**
-     * Parameters from the matched route
-     * @var array
-     */
-    protected $params = [];
+  /**
+   * Parameters from matched route
+   * @var array
+   */
 
-    /**
-     * Add a route to the routing table
-     *
-     * @param string $route  The route URL
-     * @param array  $params Parameters (controller, action, etc.)
-     *
-     * @return void
-     */
-    public function add($route, $params = [])
-    {
-        // Convert the route to a regular expression: escape forward slashes
-        $route = preg_replace('/\//', '\\/', $route);
+   protected $params = [];
+  
+   /**
+    * Add a route to routes
+    * @param string $route the route URL
+    * @param array $params Parameters (Controller, action, etc.)
 
-        // Convert variables e.g. {controller}
+    * @return void
+    */
+
+    public function add($route, $params = []) {
+
+        // convert the route to a regular expression: escape forward slashes
+        $route = preg_replace('/\//','\\/', $route);
+
+        // convert variables e.g. {ocntroller}
         $route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
 
-        // Convert variables with custom regular expressions e.g. {id:\d+}
+        // convert variable with customr egular expressions e.g. {id:\d+}
         $route = preg_replace('/\{([a-z]+):([^\}]+)\}/', '(?P<\1>\2)', $route);
 
-        // Add start and end delimiters, and case insensitive flag
         $route = '/^' . $route . '$/i';
 
         $this->routes[$route] = $params;
     }
 
     /**
-     * Get all the routes from the routing table
-     *
-     * @return array
-     */
-    public function getRoutes()
-    {
+    * Get all the routes
+    * @return array
+    */
+    public function getRoutes(){
         return $this->routes;
     }
 
     /**
      * Match the route to the routes in the routing table, setting the $params
-     * property if a route is found.
-     *
-     * @param string $url The route URL
-     *
-     * @return boolean  true if a match found, false otherwise
+     * property of a route is found
+     * 
+     * @param string $url the route URL
+     * 
+     * @return boolean true if match found, false otherwise
      */
-    public function match($url)
-    {
-        foreach ($this->routes as $route => $params) {
-            if (preg_match($route, $url, $matches)) {
-                // Get named capture group values
-                foreach ($matches as $key => $match) {
-                    if (is_string($key)) {
-                        $params[$key] = $match;
+    public function match($url){
+        foreach($this->routes as $route => $params){
+            if(preg_match($route, $url, $matches)){
+                foreach($matches as $key => $match){
+                    if(is_string($key)) {
+                        $params['key'] = $match;
                     }
                 }
 
@@ -80,113 +73,77 @@ class Router
                 return true;
             }
         }
-
         return false;
     }
 
     /**
-     * Get the currently matched parameters
-     *
+     * get the currently matched params
      * @return array
      */
-    public function getParams()
-    {
-        return $this->params;
+
+    public function getParams() { 
+        return $this->params; 
     }
 
     /**
-     * Dispatch the route, creating the controller object and running the
-     * action method
-     *
-     * @param string $url The route URL
-     *
-     * @return void
-     */
-    public function dispatch($url)
-    {
+    * Dispatch the route, creating the controller object 
+    * and running the action method
+    *
+    * @param string $url the route URL
+    *
+    * @return void
+    */
+    public function dispatch($url){
+        $this->match($url);
+
+        $helper = new Helper;
         $url = $this->removeQueryStringVariables($url);
 
-        if ($this->match($url)) {
+        if($this->match($url)){
             $controller = $this->params['controller'];
-            $controller = $this->convertToStudlyCaps($controller);
+            $controller = $helper->convertToStudlyCaps($controller);
             $controller = $this->getNamespace() . $controller;
-            
-          return $controller;
-            if (class_exists($controller)) {
+
+            if(class_exists($controller)){
                 $controller_object = new $controller($this->params);
-
+                
                 $action = $this->params['action'];
-                $action = $this->convertToCamelCase($action);
+                $action = $helper->convertToCamelCase($action);
 
-                if (preg_match('/action$/i', $action) == 0) {
-                    $controller_object->$action();
-
-                } else {
-                    // throw new \Exception("Method $action in controller $controller cannot be called directly - remove the Action suffix to call this method");
+                if(preg_match('/action$/i', $action) ==0) {
+                    try {
+                        $controller_object->$action();
+                    } catch(\Exception $e) {
+                        ("Method $action in controller $controller cannot be called directly - remove the Action suffix to call this method");
+                    }
                 }
-            } else {
-                // throw new \Exception("Controller class $controller not found");
+                // else {
+                //     throw new \Exception("Method $action in controller $controller cannot be called directly - remove the Action suffix to call this method");
+                // }
+            } 
+            else{ 
+                throw  new \Exception("Controller class $controller not found!");
             }
+
         } else {
-            // throw new \Exception('No route matched.', 404);
+            throw new \Exception('No Route Matched');
         }
+
     }
 
     /**
-     * Convert the string with hyphens to StudlyCaps,
-     * e.g. post-authors => PostAuthors
-     *
-     * @param string $string The string to convert
-     *
-     * @return string
-     */
-    protected function convertToStudlyCaps($string)
-    {
-        return str_replace(' ', '', ucwords(str_replace('-', ' ', $string)));
-    }
-
-    /**
-     * Convert the string with hyphens to camelCase,
-     * e.g. add-new => addNew
-     *
-     * @param string $string The string to convert
-     *
-     * @return string
-     */
-    protected function convertToCamelCase($string)
-    {
-        return lcfirst($this->convertToStudlyCaps($string));
-    }
-
-    /**
-     * Remove the query string variables from the URL (if any). As the full
-     * query string is used for the route, any variables at the end will need
-     * to be removed before the route is matched to the routing table. For
-     * example:
-     *
-     *   URL                           $_SERVER['QUERY_STRING']  Route
-     *   -------------------------------------------------------------------
-     *   localhost                     ''                        ''
-     *   localhost/?                   ''                        ''
-     *   localhost/?page=1             page=1                    ''
-     *   localhost/posts?page=1        posts&page=1              posts
-     *   localhost/posts/index         posts/index               posts/index
-     *   localhost/posts/index?page=1  posts/index&page=1        posts/index
-     *
-     * A URL of the format localhost/?page (one variable name, no value) won't
-     * work however. (NB. The .htaccess file converts the first ? to a & when
-     * it's passed through to the $_SERVER variable).
-     *
-     * @param string $url The full URL
-     *
-     * @return string The URL with the query string variables removed
+     * Remove Query String for url
+     * 
+     * @param string $url the full URL
+     * 
+     * @return string url without query string
      */
     protected function removeQueryStringVariables($url)
     {
-        if ($url != '') {
+        if($url != ''){
             $parts = explode('&', $url, 2);
-
-            if (strpos($parts[0], '=') === false) {
+            
+            if(strpos($parts[0], '=') === false) {
                 $url = $parts[0];
             } else {
                 $url = '';
@@ -196,20 +153,16 @@ class Router
         return $url;
     }
 
-    /**
-     * Get the namespace for the controller class. The namespace defined in the
-     * route parameters is added if present.
-     *
-     * @return string The request URL
-     */
-    protected function getNamespace()
-    {
-        $namespace = 'App\Controllers\\';
 
-        if (array_key_exists('namespace', $this->params)) {
+    public function getNamespace(){
+
+        $namespace = "App\Controllers\\";
+
+        if(array_key_exists('namespace', $this->params)) {
             $namespace .= $this->params['namespace'] . '\\';
         }
 
         return $namespace;
-    }
+      
+    }      
 }
--- public/index.php
+++ public/index.php
@@ -1,11 +1,11 @@
 <?php 
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use Core\Router;
 
 $route = new Router;
-
+die(var_dump($_SERVER['REQUEST_URI']));
 $route->add('/home/url/{id}', ['controller' => 'HomeController', 'action' => 'index']);
 $route->add('/home/url', ['controller' => 'HomeController', 'action' => 'index']);
 
