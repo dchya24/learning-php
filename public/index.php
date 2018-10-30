c83a00765f07eaa9fe1eb2b0d9b01a0ead9ea712
dchya24 <cahyadinar241@gmail.com>
2018-10-30 21:21:40 +0700

    adding route

--- public/index.php
+++ public/index.php
@@ -19,13 +19,13 @@ use Core\Router;
 use Symfony\Component\Dotenv\Dotenv;
 $data = explode('public', __DIR__);
 
+
 $env = new Dotenv();
 $env->load($data[0] . '.config');
-// die(var_dump(url('css')));
 
 $route = new Router;
 
-$route->add('', ['controller' => 'HomeController', 'action' => 'index']);
-$route->add('home/url', ['controller' => 'HomeController', 'action' => 'index']);
+$route->add('home/url', ['controller' => 'HomeController', 'action' => 'noParams']);
+$route->add('home/url/{id:\d+}', ['controller' => 'HomeController', 'action' => 'index']);
 
 $route->dispatch($_SERVER['QUERY_STRING']);
