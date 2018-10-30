8bc07555fc50b687c841042fa1d22174c85be764
dchya24 <cahyadinar241@gmail.com>
2018-10-30 21:21:21 +0700

    adding function

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -5,7 +5,12 @@ namespace App\Controllers;
 use Core\View;
 
 class HomeController {
-    public function index(){
-        View::renderTemplate('home.html', ['data' => 'data aku']);
+    public function index($id){
+        die(var_dump($id));
+        // View::renderTemplate('home.html', ['data' => 'data aku']);
+    }
+
+    public function noParams(){
+        die(var_dump("noParams"));
     }
 }
